d1c85492fa02478f58611446db4f78b37cac46e6
Pedro Jorge <pedrojorge2011@gmail.com>
2024-08-18 17:19:59 -0300

    add colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,25 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Roboto', 'sans-serif'],
-        baloo: ['"Baloo 2"']
+        alt: ['"Baloo 2"']
+      },
+      colors: {
+        yellowdark: 'var(--yellow-dark)',
+        yellow: 'var(--yellow)',
+        yellowligth: 'var(--yellow-ligth)',
+        purpledark: 'var(--purple-dark)',
+        purple: 'var(--purple)',
+        purpleligth: 'var(--purple-ligth)',
+        basetitle: 'var(--base-title)',
+        basesubtitle: 'var(--base-subtitle)',
+        basetext: 'var(--base-text)',
+        baselabel: 'var(--base-label)',
+        basehover: 'var(--base-hover)',
+        basebutton: 'var(--base-button)',
+        baseinput: 'var(--base-input)',
+        basecard: 'var(--base-card)',
+        background: 'var(--background)',
+        white: 'var(--white)'
       }
     },
   },
